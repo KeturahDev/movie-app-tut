689de6dbc002aa130ace22835184fe339081a9ac
Keturah Howard <keturah.dev@gmail.com>
2025-01-19 00:36:23 -0800

    add styles

--- frontend/src/pages/Favorites.tsx
+++ frontend/src/pages/Favorites.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "../styles/Favorites.css";
 
 const Favorites = () => {
   return (
     <div>
-      <h3 className="page_title">Favorites</h3>
-      <div className="fave-empty">
+      <div className="faves-empty">
         <h2>No Favorites yet</h2>
         <p>Start favoriting and it will appear here</p>
       </div>
