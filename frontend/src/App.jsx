d55d9f8a9419c476fc425b931dd1c93b16de2e5d
Keturah Howard <keturah.dev@gmail.com>
2025-01-19 00:22:47 -0800

    add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,21 @@ import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-brand">
+        <Link to={"/"}>Movie App</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/Favorites"}>Favorites</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
